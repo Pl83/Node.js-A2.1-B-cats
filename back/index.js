e1f94e772a070a1993e093fa4e66024f0bd90677
Nalithdev <Ethan.delcroix@outlook.fr>
2023-01-26 11:23:17 +0100

    initialisation du back

---
+++ back/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors= require('cors');
+const http = require('http');
+const socketio = require('socket.io');
+
+const app = express();
+const port = 3000;
+
+const serverhttp = http.createServer(app);
+const io = socketio(serverhttp);
+serverhttp.use(express.json());
+serverhttp.use(cors());
+serverhttp.use(http())
+const io = socketio(serverhttp , {
+    cors: {
+        origin: '*',
+    }
+});
