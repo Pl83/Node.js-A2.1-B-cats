96190efbdb926b4069b1ca3ded0a7f73993633ed
DalSchim <cabrillac123@gmail.com>
2023-01-28 17:39:41 +0100

    req information in form

---
+++ front/loin.js
@@ -0,0 +1,15 @@
+function register(){
+    let user = document.querySelector("#userr").value;
+    let email = document.querySelector("#email").value;
+    let password = document.querySelector("#passe").value;
+    let pokfav= document.querySelector("#favor").value;
+}
+
+function login(){
+    let user = document.querySelector("#userr").value;
+    let password = document.querySelector("#passe").value;
+}
+
+
+
+
