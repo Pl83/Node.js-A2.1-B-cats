ab876250bfe4bf499cc2d389c2170e413e5f3d6a
DalSchim <cabrillac123@gmail.com>
2023-01-29 15:17:49 +0100

    delete email input and coretion id for login fuction

--- front/loin.js
+++ front/loin.js
@@ -1,12 +1,11 @@
 function register(){
     let user = document.querySelector("#userr").value;
-    let email = document.querySelector("#email").value;
     let password = document.querySelector("#passe").value;
     let pokfav= document.querySelector("#favor").value;
 }
 
 function login(){
-    let user = document.querySelector("#userr").value;
+    let user = document.querySelector("#userl").value;
     let password = document.querySelector("#passe").value;
 }
 
